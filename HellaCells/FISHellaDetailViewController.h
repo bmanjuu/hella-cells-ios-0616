18922aaf332a6533e895254a8355e106c27a8722
bmanjuu <bettyofung@gmail.com>
2016-06-22 10:07:01 -0400

    hella cells complete

---
+++ HellaCells/FISHellaDetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  FISHellaDetailViewController.h
+//  HellaCells
+//
+//  Created by Betty Fung on 6/21/16.
+//  Copyright © 2016 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISHellaDetailViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *bigNumber;
+@property (nonatomic) NSString *number;
+
+@end
